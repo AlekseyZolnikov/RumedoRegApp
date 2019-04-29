95a2de9b3aeb655e5df13c3a5598f6327134efcf
aleksej <monsarda@yandex.ru>
2019-04-29 16:59:03 +0300

    stage sensor work

--- app/src/main/java/ru/rumedo/rumedoregapp/MainActivity.java
+++ app/src/main/java/ru/rumedo/rumedoregapp/MainActivity.java
@@ -1,8 +1,13 @@
 package ru.rumedo.rumedoregapp;
 
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -11,15 +16,35 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     private static final String TAG = "MainActivityTag";
+    private SensorManager sensorManager;
+    private List<Sensor> sensors;
+    private Sensor temperature;
+    private Sensor humidity;
+    private MainFragment mainFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensors = sensorManager.getSensorList(Sensor.TYPE_ALL);
+        temperature = sensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE);
+        humidity = sensorManager.getDefaultSensor(Sensor.TYPE_RELATIVE_HUMIDITY);
+
+        Bundle bundle = new Bundle();
+        bundle.putString(MainFragment.KEY_BUNDLE_SENSORS, getSensors());
+        mainFragment = new MainFragment();
+        showFragment(mainFragment);
+
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -33,6 +58,36 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(sensorHumidityListener, humidity, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(sensorTempListener, temperature, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    private String getSensors() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (Sensor sensor : sensors) {
+            stringBuilder
+                    .append("name = ").append(sensor.getStringType()).append("\n");
+        }
+
+        return stringBuilder.toString();
+    }
+
+    private void showFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.main_activity_frame_layout, fragment)
+                .commit();
+    }
+
+    private void showFragment(Fragment fragment, Bundle bundle) {
+        fragment.setArguments(bundle);
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.main_activity_frame_layout, fragment)
+                .commit();
     }
 
     @Override
@@ -66,4 +121,41 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+    private void showTemperatureSensors(SensorEvent event) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Temp Sensor value = ").append(event.values[0]).append("\n");
+        mainFragment.setTemperatureValue(stringBuilder.toString());
+    }
+
+    private void showHumiditySensors(SensorEvent event) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Humidity Sensor value = ").append(event.values[0]).append("\n");
+        mainFragment.setHumidityValue(stringBuilder.toString());
+
+    }
+
+    private final SensorEventListener sensorHumidityListener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            showHumiditySensors(event);
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+        }
+    };
+
+    private final SensorEventListener sensorTempListener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            showTemperatureSensors(event);
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+        }
+    };
 }
---
+++ app/src/main/java/ru/rumedo/rumedoregapp/MainFragment.java
@@ -0,0 +1,48 @@
+package ru.rumedo.rumedoregapp;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MainFragment extends Fragment {
+
+
+    public static final String KEY_BUNDLE_SENSORS = "KEY_BUNDLE_SENSORS";
+    private TextView tempSensor;
+    private TextView humiditySensor;
+
+    public MainFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        tempSensor = view.findViewById(R.id.temp_sensor);
+        humiditySensor = view.findViewById(R.id.humidity_sensor);
+
+        return view;
+    }
+
+    public void setTemperatureValue(String value) {
+        tempSensor.setText(value);
+    }
+
+    public void setHumidityValue(String value) {
+        humiditySensor.setText(value);
+    }
+
+
+
+}
