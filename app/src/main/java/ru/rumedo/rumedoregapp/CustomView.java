effa38fccc7210d9fe2b867f18e7b3a023076a41
aleksej <monsarda@yandex.ru>
2019-04-30 12:17:41 +0300

    light refactor

--- app/src/main/java/ru/rumedo/rumedoregapp/CustomView.java
+++ app/src/main/java/ru/rumedo/rumedoregapp/CustomView.java
@@ -9,7 +9,6 @@ import android.graphics.Paint;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
 
 public class CustomView extends View {
