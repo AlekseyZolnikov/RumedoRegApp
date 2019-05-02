84c79d0f2e43d5d9b8b386fd58c9a99922198500
LSDance <monsarda@yandex.ru>
2019-05-02 22:34:01 +0300

    create user service

---
+++ app/src/main/java/ru/rumedo/rumedoregapp/entity/User.java
@@ -0,0 +1,4 @@
+package ru.rumedo.rumedoregapp.entity;
+
+public class User {
+}
---
+++ app/src/main/java/ru/rumedo/rumedoregapp/service/UserService.java
@@ -0,0 +1,24 @@
+package ru.rumedo.rumedoregapp.service;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.support.annotation.Nullable;
+
+import java.util.List;
+
+import ru.rumedo.rumedoregapp.entity.User;
+
+public class UserService extends IntentService {
+
+    public UserService() {
+        super("UserService");
+    }
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+
+    }
+
+    private List<User> getUserList() {}
+
+}
