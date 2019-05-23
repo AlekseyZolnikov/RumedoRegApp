f02e115329aa97a5a69d6cc7670e2b6cae0c7726
aleksej <monsarda@yandex.ru>
2019-05-23 14:50:19 +0300

    create dbhelper

---
+++ app/src/main/java/ru/rumedo/rumedoregapp/database/DatabaseHelper.java
@@ -0,0 +1,52 @@
+package ru.rumedo.rumedoregapp.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.BaseColumns;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "users.db";
+    private static final int DATABASE_VERSION = 2;
+
+    public static final String TABLE_USERS = "users";
+
+    public static final String COLUMN_ID = BaseColumns._ID;
+    public static final String COLUMN_NAME = "name";
+    public static final String COLUMN_SURNAME = "surname";
+    public static final String COLUMN_EMAIL = "email";
+    public static final String COLUMN_PHONE = "phone";
+    public static final String COLUMN_EVENT = "event";
+    public static final String COLUMN_REGDATE = "regdate";
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(
+            "CREATE TABLE "
+                + TABLE_USERS
+                + "( "
+                    + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                    + COLUMN_NAME + " TEXT, "
+                    + COLUMN_SURNAME + " TEXT, "
+                    + COLUMN_EMAIL + " TEXT, "
+                    + COLUMN_PHONE + " TEXT, "
+                    + COLUMN_EVENT + " TEXT, "
+                    + COLUMN_REGDATE + " TEXT" +
+                ");"
+        );
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        if ((oldVersion == 1) && (newVersion == 2)) {
+            String upgradeQuery = "ALTER TABLE " + TABLE_USERS + " ADD COLUMN "
+                    + COLUMN_EVENT + " TEXT DEFAULT 'EVENT'";
+            db.execSQL(upgradeQuery);
+        }
+    }
+}
